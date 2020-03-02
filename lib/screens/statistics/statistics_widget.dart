b5103b61aaebdacdf4475c669c32afcebc85f1aa
Mantab Tadayon <mt.tadayon@gmail.com>
2020-03-02 15:23:53 +0100

    Create card for general information

--- lib/screens/statistics/statistics_widget.dart
+++ lib/screens/statistics/statistics_widget.dart
@@ -1,8 +1,21 @@
+import 'package:covid_19/Widgets/card_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_icons/flutter_icons.dart';
 
 class StatisticsWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ListView(
+      children: <Widget>[
+        CardWidget(
+          icon: Icon(FontAwesome5Solid.globe),
+          title: 'Globale Statistics',
+          text: '''Infections: 30000
+Death: 2000
+Recoverd: 15874
+        ''',
+        ),
+      ],
+    );
   }
 }
