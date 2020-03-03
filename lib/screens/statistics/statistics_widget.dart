c01c843dbec537539099e600e74622952c3c21cd
Mantab Tadayon <mt.tadayon@gmail.com>
2020-03-03 17:38:08 +0100

    Information date update

--- lib/screens/statistics/statistics_widget.dart
+++ lib/screens/statistics/statistics_widget.dart
@@ -4,6 +4,7 @@ import 'package:covid_19/generated/l10n.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_icons/flutter_icons.dart';
 import 'package:covid_19/Service/fetch_data.dart';
+import 'package:intl/intl.dart';
 
 class StatisticsWidget extends StatefulWidget {
   @override
@@ -30,6 +31,10 @@ class _StatisticsWidgetState extends State<StatisticsWidget> {
           (BuildContext context, AsyncSnapshot<List<CountryModel>> snapshot) {
         if (snapshot.hasData) {
           return ListView(children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text('*Information from ${DateFormat.yMd().format(DateTime.now())} - UTC +1'),
+            ),
             CardWidget(
               icon: Icon(FontAwesome5Solid.globe),
               title: S.of(context).globalStatisticTitle,
